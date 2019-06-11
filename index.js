440a86f99d52d041279694599a5ce958fa6ab900
Nat-wang <44423052+Nat-wang@users.noreply.github.com>
2019-06-11 16:22:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,42 +2,90 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Test extends React.Component{
+class App extends React.Component{
   constructor(props){
     super(props);
-    this.state={
-      value: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  this.state={
+    items:[],
+    text:''
   }
-  handleChange(e){
-    this.setState({value: e.target.value});
-  }
-  handleSubmit(e){
-    //Send to server
-    //___.emit("command", e.target.value)
-    console.log("Sent code to server");
+  this.handleAddTodo=this.handleAddTodo.bind(this);
+  this.handleChange=this.handleChange.bind(this);
+  this.handleDeleteTodo=this.handleDeleteTodo.bind(this);
+
+};
+  handleDeleteTodo(e){
     e.preventDefault();
+    this.setState(state=>({
+      items: state.items.splice()
+    }));
+  }
+  handleAddTodo(e){
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now(),
+      status: false
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
   }
 
+setStrike(e){
+  e.preventDefault();
+
+}
+
+  handleChange(e){
+    this.setState({text: e.target.value})
+  };
   render(){
-      return(
-        <section>
-          <form id="main"onSubmit={this.handleSubmit}>
-          <label id="box_label">
-          Type your code here:
-          <br/>
-              <textarea id="box"className="main_box"rows="10" cols="60"value={this.state.value} onChange={this.handleChange}>
-              </textarea>
-              <input id="box_btn"className="main_button"type="submit" value="Run Code"/>
-          </label>
-          </form>
-        </section>
-      );
+    return(
+      <div>
+      <p>Todo List</p>
+      <TodoList items={this.state.items}/>
+      <form onSubmit={this.handleAddTodo}>
+
+      <input type="text"
+        value={this.state.text}
+        onChange={this.handleChange}/>
+        <input type="button"
+          value="Add Todo"
+          onClick={this.handleAddTodo}/>
+      </form>
+
+      <button
+        onClick={this.handleDeleteTodo}>Delete</button>
+      </div>
+    )
+}
+}
+class TodoList extends React.Component {
+
+  render() {
+    return (
+
+      <div>
+        {this.props.items.map(item => (
+          <div>
+          <input id="checked"onClick={App.setStrike}type="checkbox"/>
+          <p id="todo"key={item.id}>{item.text}</p>
+
+          </div>
+        ))}
+      </div>
+
+    );
+
   }
-};
+}
+
 ReactDOM.render(
-  <Test/>,
+  <App/>,
   document.getElementById("root")
 );
